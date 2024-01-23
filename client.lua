7ab7145289248d883f416e292d4411bc6d3ed334
Gangstaaaaa <85770558+Gangstaaaaa@users.noreply.github.com>
2024-01-23 10:01:00 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -2,18 +2,10 @@ function openGui(color,message,time)
     guiEnabled = true
     SendNUIMessage({runProgress = true, colorsent = color, textsent = message, fadesent = time})
 end
+exports('sendnotify', openGui)
 
 function closeGui()
     guiEnabled = false
     SendNUIMessage({closeProgress = true})
 end
-
-RegisterNetEvent("notifications:sendNotification")
-AddEventHandler("notifications:sendNotification", function(color,message,time)
-    openGui(color,message,time)
-end)
-
-RegisterNetEvent("notifications:closeUI")
-AddEventHandler("notifications:closeUI", function()
-    closeGui()
-end)
+exports('closenotify', closeGui)
