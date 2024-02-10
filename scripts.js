10c0a562133e0ada68ca661523127e8422a17890
Gangstaaaaa <85770558+Gangstaaaaa@users.noreply.github.com>
2024-02-10 04:21:31 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -86,7 +86,7 @@ var warningSvg = `<svg width="40" height="42" viewBox="0 0 40 42" fill="none" xm
       var fadetimer = item.fadesent
       var element
       $('#colorsent' + item.colorsent).css('display', 'none');
-      if (item.colorsent == 'error) {
+      if (item.colorsent == 'error') {
         element = $('<div id="colorsent' + item.colorsent + '" class="notification-bg red" style="display:none" data-total-time="' + fadetimer + '"><i class="icon">' + errorSvg + "</i>" + message + notifyTimer + '</div>'); 
       } else if (item.colorsent == 'warning') {
         element = $('<div id="colorsent' + item.colorsent + '" class="notification-bg taxi" style="display:none" data-total-time="' + fadetimer + '"><i class="icon">' + warningSvg + "</i>" + message + notifyTimer + '</div>');
